6de9de619fdf90f6caa90b8994c7b82207b8fade
baoren <xdman1221@gmail.com>
2023-02-14 18:01:17 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokersForFun.Models;
+
+namespace JokersForFun.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokersForFun.Models.Joke> Joke { get; set; }
+    }
+}
